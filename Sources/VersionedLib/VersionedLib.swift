33afaa4fe0a00a716b2b1ac3ae0f532c8b6d3c4c
Dmitrii Shelonin <cheisesu@gmail.com>
2026-01-08 00:10:33 +0100

    - added some version

---
+++ Sources/VersionedLib/VersionedLib.swift
@@ -0,0 +1,12 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+@available(VersionedLib 1.0.0, *)
+public final class SomeClass {
+    let name: String
+    public init(_ name: String) {
+        self.name = name
+    }
+}
